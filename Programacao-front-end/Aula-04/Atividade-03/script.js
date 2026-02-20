2f85278ab5e0aa00121c7989037afb51a7cabeb0
LuanS <luan.ebaaaa@gmail.com>
2026-02-20 08:58:28 -0300

    atividade 3 sofrimento

--- Programacao-front-end/Aula-04/Atividade-03/script.js
+++ Programacao-front-end/Aula-04/Atividade-03/script.js
@@ -1,10 +1,26 @@
-function cadastro(){
-    let NewPessoa = document.createElement("li");
-    let name = document.getElementById("email").value;
-    let email = document.getElementById("name").value;
-    let rm = document.getElementById("rm").value;
-    let tele = document.getElementById("tele").value;
-    let turma = document.getElementById("turma").value;
+    function cadastro(){
+        let Treco = document.createElement("li");
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let rm = document.getElementById("rm").value;
+        let tele = document.getElementById("tele").value;
+        let turma = document.getElementById("turma").value;
 
-    NewPessoa.textContent = name;
-}
+        Treco.innerHTML = name + "<br>" + email + "<br>" + rm + "<br>" + tele + "<br>" + turma; 
+        document.getElementById("NewPessoa").appendChild(Treco);
+        NewPessoa.appendChild(Treco)
+
+          let botaoRemover = document.createElement("button");
+        botaoRemover.textContent = "Remover Aluno";
+        
+        botaoRemover.onclick = function() {
+        Treco.remove();
+        }
+        
+        Treco.appendChild(botaoRemover);
+        document.getElementById("NewPessoa").appendChild(Treco);
+    }
+
+
+
+       
